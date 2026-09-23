c07896ac689d84d5b88e2b4d6933456294971c0d
mbmorandi <mmorandi@horizon.csueastbay.edu>
2026-09-22 18:29:00 -0700

    Add comments for semester setup and student management
    Added comments for future implementation steps in main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include "Menu.h"
 #include "Semester.h"
 int main(){
+    //if input file exists, upload the input file
+    //set up a new semester
+    //add students
+    //add student grades
+    //file in anything else needed.
     Menu menu;
     menu.openMenu();
 }
